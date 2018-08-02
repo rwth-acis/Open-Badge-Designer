0f72881c4d467079585713e2b4dc944297c22434
schruff <schruff@dbis.rwth-aachen.de>
2018-08-02 21:11:20 +0200

    changes to xAPI access (unfinished)

--- Backend/src/main/java/ApplicationController.java
+++ Backend/src/main/java/ApplicationController.java
@@ -20,35 +20,65 @@ public class ApplicationController {
     }
 
     @CrossOrigin()
+    @GetMapping("/checkxapiconnection")
+    public String checkXAPIConnection(@RequestParam(required=false, defaultValue="") String url, 
+            @RequestParam(required=false, defaultValue="") String auth){
+            
+        if(url == "" || auth == "")
+            return "Insufficient Parameters";
+        System.out.println("External Component has made an xAPI connection check.");
+        
+        Response response = sendXPIRequest(url, "", auth);
+        
+        if(response == null){
+            return "Connection Failed.";
+        }else{
+            return "Connection Succeeded.";
+        }
+    }
+    
+    @CrossOrigin()
     @GetMapping("/xapitest")
     public String xAPItest(){
-	OkHttpClient client = new OkHttpClient();
-	System.out.println("Attempting to request xAPI statements.");
-	Request request = new Request.Builder()
-	  .url("http://localhost/data/xAPI/statements?since=2018-07-28T00:01:00.360Z")
-	  .get()
-	  .addHeader("X-Experience-API-Version", "1.0.1")
-	  .addHeader("Authorization", "Basic MTZlODUyNzllYTQ5YzA5YTkzNGE2N2RhOWQzMjQ5M2Y1YTI1OTc5MjpjYWM3YTExYTJhY2E0N2Y2YjMxMDI4YjhkNjA3MTg4MjM2NTk0Y2Yy")
-	  .addHeader("Cache-Control", "no-cache")
-	  .build();
-	Response response;
-	String body = "uninitialized";
-	try{
-	    response = client.newCall(request).execute();
-	}catch(Exception e){
-	    System.out.println("Something went wrong");
-	    response = null;
-	}
-	try{
-	    body = response.body().string();
-	}catch(Exception e){
-	    System.out.println("String of body could not be determined.");
-	}
-	try{
-	    return body;
-	}catch(Exception e){
-	    return "Failed.";
-	}
+    
+        Response response = sendXAPIRequest("http://localhost/data/xAPI/statements", "?since=2018-07-28T00:01:00.360Z",
+                "Basic MTZlODUyNzllYTQ5YzA5YTkzNGE2N2RhOWQzMjQ5M2Y1YTI1OTc5MjpjYWM3YTExYTJhY2E0N2Y2YjMxMDI4YjhkNjA3MTg4MjM2NTk0Y2Yy");
+	    String body = "uninitialized";
+	    try{
+	        response = client.newCall(request).execute();
+	    }catch(Exception e){
+	        System.out.println("Something went wrong");
+	        response = null;
+	    }
+	    try{
+	        body = response.body().string();
+	    }catch(Exception e){
+	        System.out.println("String of body could not be determined.");
+	    }
+	    try{
+	        return body;
+	    }catch(Exception e){
+	        return "Failed.";
+	    }
+    }
+    
+    public static Response sendXAPIRequest(String url, String query, String auth){
+        OkHttpClient client = new OkHttpClient();
+        System.out.println("Attempting to send xAPI request.");
+        Request request = new Request.Builder()
+            .url(url+query)
+            .get()
+            .addHeader("X-Experience-API-Version", "1.0.1")
+            .addHeader("Authorization", auth)
+            .addHeader("Cache-Control", "no-cache")
+            .build();
+        try{
+            return client.newCall(request).execute();
+        catch(Exception e){
+            e.printStckTrace();
+            return null;
+        }
+        
     }
 
 }
--- Frontend/public/frontend.js
+++ Frontend/public/frontend.js
@@ -9,12 +9,13 @@ function constructUI(){
     // check location within HTML page to start building elements
     var location = document.getElementById("frontendUI");
     
+    // prepare divisions
     var left = document.createElement("div");
     var right = document.createElement("div");
-
     location.appendChild(left);
     location.appendChild(right);
-
+    
+    // set style for UI (CSS)
     left.style.backgroundColor = 'papayawhip';
     left.style.width = '50%';
     left.style.left = 0;
@@ -24,6 +25,7 @@ function constructUI(){
     right.style.right = 0;
     right.style.position = 'fixed';
 
+    // prepare paragraphs
     var backendtextlocation = document.createElement("p");
     var backendfieldlocation = document.createElement("p");
     var backendbuttonlocation = document.createElement("p");
@@ -32,54 +34,54 @@ function constructUI(){
     var authorizetextlocation = document.createElement("p");
     var authorizefieldlocation = document.createElement("p");
     
-    left.appendChild(backendtextlocation);
-    left.appendChild(backendfieldlocation);
-    left.appendChild(backendbuttonlocation);
-    right.appendChild(urltextlocation);
-    right.appendChild(urlfieldlocation);
-    right.appendChild(authorizetextlocation);
-    right.appendChild(authorizefieldlocation);
+    // assign paragraphs to divisions
+    appendChildren(left, backendtextlocation, backendfieldlocation, backendbuttonlocation);
+    appendChildren(right, urltextlocation, urlfieldlocation, authorizetextlocation, authorizefieldlocation);
 
-    // create a field to enter the URL of the Backend
+    // create elements for entering the URL of the Backend
     var backendtext = document.createTextNode("Step 1 (necessary): connect to a Backend instance");
-    backendtextlocation.append(backendtext);
     var backendfield = document.createElement("INPUT");
     setAttributes(backendfield,
             "type", "text",
             "placeholder", "http://localhost:9000",
             "id", "backendfield");
-    backendfieldlocation.appendChild(backendfield);
     var backendbutton = document.createElement("BUTTON");
     backendbutton.appendChild(document.createTextNode("Test Connection"));
     backendbutton.onclick = checkConnection;
-    backendbuttonlocation.appendChild(backendbutton);
 
-    // create a field to enter the URL of LRS
+    // create elements for entering the URL of LRS
     var urlfield = document.createElement("INPUT");
     setAttributes(urlfield,
             "type", "text",
             "placeholder", "http://localhost/data/xAPI/statements");
-    urlfieldlocation.appendChild(urlfield);
     var urltext = document.createTextNode("Add URL of the statements section of your LRS here: ");
-    urltextlocation.appendChild(urltext);
     
-    // create a field to enter authorization for LRS
+    // create elements for entering authorization for LRS
     var authorizefield = document.createElement("INPUT");
     setAttributes(authorizefield,
             "type", "text",
             "placeholder", "Basic MTZlODUyNzllYTQ5YzA5...");
-    authorizefieldlocation.appendChild(authorizefield);
     var authorizetext = document.createTextNode("Add HTTP authorization for the LRS");
+    
+    // add elements to their respective paragraphs / divisions
+    backendtextlocation.append(backendtext);
+    backendfieldlocation.appendChild(backendfield);
+    backendbuttonlocation.appendChild(backendbutton);
+    urltextlocation.appendChild(urltext);
+    urlfieldlocation.appendChild(urlfield);
+    authorizefieldlocation.appendChild(authorizefield);
     authorizetextlocation.appendChild(authorizetext);
 }
 
 // function from stack overflow (license needed?)
+// apply several attribute, value pairs at once
 function setAttributes(elem /*additional attribute, value pairs can be passed*/){
     for (var i = 1; i < arguments.length; i+=2){
         elem.setAttribute(arguments[i], arguments[i+1]);
     }
 }
 
+// append several children at once
 function appendChildren(elem /* children can be passed here */){
     for (var i=1; i < arguments.length; i+=1){
         elem.appendChild(arguments[i]);
